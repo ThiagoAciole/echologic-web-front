3c3c3093dcddd67d151a212297fd75cd423efc87
ThiagoAciole <thiagoaciole7@gmail.com>
2023-05-23 22:32:44 -0300

    feat(#3):"Base Tela Inicial & Resposividade"

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,39 @@
-
-
+import { useState } from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
 export default function Header() {
-    return (
-        <div className=" h-w-full bg-white-500 border-y p-6 shadow">
-            <div className="flex text-2xl">
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header>
+      <nav className={menuOpen ? 'open' : ''}>
+        <div className="logo">
+        <div className="flex text-2xl">
               <h1 className="font-extrabold text-lime-500">Echo</h1>
               <h1 className="text-gray-500">Logic</h1>
             </div>
-           
         </div>
-    )
+        <ul className="menu">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/sobre">Sobre</Link></li>
+          <li><Link to="/legislacao">Legislação</Link></li>
+        </ul>
+        <button className="btn">Materiais</button>
+        {menuOpen && (
+          <button className="back-btn" onClick={handleMenuToggle}>
+            Voltar
+          </button>
+        )}
+        <div className="menu-toggle" onClick={handleMenuToggle}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+    </header>
+  );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    // Path to the Tremor module
+"./node_modules/@tremor/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
