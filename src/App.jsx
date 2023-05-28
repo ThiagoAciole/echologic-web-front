2f11d337cb0b7ce6213d8b195ab43787343f31a9
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2023-05-28 00:37:05 -0300

    feat(#15): Tela vidro

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home/Home';
 import TelaPadrao from './pages/TelaPadrao/TelaPadrao';
 import Ouro from './pages/Ouro/Ouro'
 import Zinco from './pages/Zinco/Zinco'
+import Vidro from './pages/Vidro/Vidro';
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                   <Route path='tela-padrao' element={<TelaPadrao />} />
                   <Route path='ouro' element={<Ouro/>} />
                   <Route path='zinco' element={<Zinco/>} />
+                  <Route path='vidro' element={<Vidro/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
