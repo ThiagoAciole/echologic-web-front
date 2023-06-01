b34ba19e4dc80a9e0317295f05f2becb963faced
ThiagoAciole <thiagoaciole7@gmail.com>
2023-06-01 18:21:03 -0300

    responsive fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,11 +20,13 @@ export default function Home() {
         
     </div>
     <div id="lixoEletronico" className="mt-24 h-full items-center flex flex-col sm:flex-col space-y-8">
-        <div className="space-y-8 sm:w-1/2 sm:p-8">
+        <div className="space-y-8 sm:w-1/2 sm:p-8 mx-auto max-w-xl">
             <h1 className="font-sans text-5xl font-extrabold text-gray-700 text-center">Conheça os Materiais que degradam o meio ambiente</h1>
             <p className="font-sans text-xl text-gray-700 text-center">
-            Conheça os principais materiais utilizados na fabricação de equipamentos de informática e seu impacto ambiental    </p>
-            </div>
+            Conheça os principais materiais utilizados na fabricação de equipamentos de informática e seu impacto ambiental
+            </p>
+        </div>
+
             <div className="flex flex-col space-y-6 sm:flex-row sm:space-y-0 sm:space-x-6 ml-12 mr-12">
             <CardMaterial 
             name="Ouro" 
@@ -54,13 +56,14 @@ export default function Home() {
     <div>
   
     </div>
-        <div id="maisInfo" className="mt-24 h-full items-center flex flex-col sm:flex-col space-y-8 text-center ml-12 mr-12">
-            <h1 className="font-sans text-5xl font-extrabold text-gray-700 w-1/2 ">Mais Informações Sobre o Lixo Eletronico e Seu devido Descarte</h1>
-            <p className="font-sans text-xl text-gray-700">
-            Descubra tudo sobre o descarte correto do lixo eletrônico e a legislação que o envolve. Entenda por que é tão importante agir de forma responsável ao se livrar de seus dispositivos antigos. Saiba como você pode contribuir para a proteção do meio ambiente e evitar problemas legais. Clique aqui para obter mais informações sobre o tema e fique por dentro de todas as orientações e regulamentações relacionadas ao descarte adequado de lixo eletrônico.
-            </p>
-            <button className="btn">Saiba Mais</button>
-    </div>
+    <div id="maisInfo" className="mt-24 h-full items-center flex flex-col space-y-8 text-center mx-auto max-w-xl">
+  <h1 className="font-sans text-5xl font-extrabold text-gray-700 w-full sm:w-1/2">Mais Informações Sobre o Lixo Eletrônico e Seu devido Descarte</h1>
+  <p className="font-sans text-xl text-gray-700">
+    Descubra tudo sobre o descarte correto do lixo eletrônico e a legislação que o envolve. Entenda por que é tão importante agir de forma responsável ao se livrar de seus dispositivos antigos. Saiba como você pode contribuir para a proteção do meio ambiente e evitar problemas legais. Clique aqui para obter mais informações sobre o tema e fique por dentro de todas as orientações e regulamentações relacionadas ao descarte adequado de lixo eletrônico.
+  </p>
+  <button className="btn">Saiba Mais</button>
+</div>
+
       </>
     )
     
--- src/pages/Home/Organização/colaboradores.jsx
+++ src/pages/Home/Organização/colaboradores.jsx
@@ -1,51 +1,40 @@
-
-
-
 export default function Colaboradores() {
-    
-  return (
-  
-    <div >
+    return (
+      <div className="mx-auto max-w-xl">
         <h1 className="font-sans text-3xl font-extrabold text-gray-700 text-center">Colaboradores do projeto</h1>
-        <div className="mt-12 flex flex-row space-x-12">
-            
-            
-            <div className="space-y-2">
-            <img src="https://avatars.githubusercontent.com/u/64097786?v=4" width="150px;" alt="Avatar Thiago" className="rounded-full"/>
+        <div className="mt-12 flex flex-col sm:flex-row sm:space-x-4">
+          <div className="space-y-2 flex flex-col items-center">
+            <img src="https://avatars.githubusercontent.com/u/64097786?v=4" width="150px" alt="Avatar Thiago" className="rounded-full"/>
             <h1 className="font-sans font-extrabold text-gray-500 text-center">Thiago Aciole</h1>
             <p className="font-sans text-gray-700 text-center">Aluno</p>
-            </div>
-            <div className="space-y-2">
-            <img src="https://avatars.githubusercontent.com/u/40415279?v=4"width="150px;" alt="Avatar Thiago" className="rounded-full"/>
+          </div>
+          <div className="space-y-2 flex flex-col items-center">
+            <img src="https://avatars.githubusercontent.com/u/40415279?v=4" width="150px" alt="Avatar Richard" className="rounded-full"/>
             <h1 className="font-sans font-extrabold text-gray-500 text-center">Richard Ferreira</h1>
             <p className="font-sans text-gray-700 text-center">Aluno</p>
-            </div>
-            <div className="space-y-2">
-            <img src="https://avatars.githubusercontent.com/u/93728229?v=4" width="150px;" alt="Avatar Thiago" className="rounded-full"/>
+          </div>
+          <div className="space-y-2 flex flex-col items-center">
+            <img src="https://avatars.githubusercontent.com/u/93728229?v=4" width="150px" alt="Avatar Liedson" className="rounded-full"/>
             <h1 className="font-sans font-extrabold text-gray-500 text-center">Liedson Augusto</h1>
             <p className="font-sans text-gray-700 text-center">Aluno</p>
-            </div>
-            <div className="space-y-2">
-            <img src="https://avatars.githubusercontent.com/u/90874749?v=4" width="150px;" alt="Avatar Thiago" className="rounded-full"/>
+          </div>
+          <div className="space-y-2 flex flex-col items-center">
+            <img src="https://avatars.githubusercontent.com/u/90874749?v=4" width="150px" alt="Avatar Emerson" className="rounded-full"/>
             <h1 className="font-sans font-extrabold text-gray-500 text-center">Emerson Ian</h1>
             <p className="font-sans text-gray-700 text-center">Aluno</p>
-            </div>
-            <div className="space-y-2">
-            <img src="https://avatars.githubusercontent.com/u/93790229?v=4" width="150px;" alt="Avatar Thiago" className="rounded-full"/>
+          </div>
+          <div className="space-y-2 flex flex-col items-center">
+            <img src="https://avatars.githubusercontent.com/u/93790229?v=4" width="150px" alt="Avatar Ynnayron" className="rounded-full"/>
             <h1 className="font-sans font-extrabold text-gray-500 text-center">Ynnayron Juan</h1>
             <p className="font-sans text-gray-700 text-center">Aluno</p>
-            </div>
-            <div className="space-y-2">
-            <img src="https://avatars.githubusercontent.com/u/87623117?v=4" width="150px;" alt="Avatar Thiago" className="rounded-full"/>
+          </div>
+          <div className="space-y-2 flex flex-col items-center">
+            <img src="https://avatars.githubusercontent.com/u/87623117?v=4" width="150px" alt="Avatar Ivysson" className="rounded-full"/>
             <h1 className="font-sans font-extrabold text-gray-500 text-center">Ivysson Uchoa</h1>
             <p className="font-sans text-gray-700 text-center">Aluno</p>
-            </div>
-           
-            </div>
-            
-        
-        
-    </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   
-  );
-}
