c30725c4246c07fc51ed66158488e089345bf8a7
IvyssonUchoa <ivysson2312@gmail.com>
2023-06-01 19:31:55 -0300

    feat(#7) Cria Tela do Mercúrio

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import TelaPadrao from './pages/TelaPadrao/TelaPadrao';
 import Ouro from './pages/Ouro/Ouro'
 import Zinco from './pages/Zinco/Zinco'
 import Vidro from './pages/Vidro/Vidro';
+import Mercurio from './pages/Mercurio/Mercurio';
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
                   <Route path='ouro' element={<Ouro/>} />
                   <Route path='zinco' element={<Zinco/>} />
                   <Route path='vidro' element={<Vidro/>} />
+                  <Route path='mercurio' element={<Mercurio/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
